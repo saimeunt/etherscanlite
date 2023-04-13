dd80aa8943f97cf1a75671c367908145b7b6944b
Simon <saimeunt@gmail.com>
2023-04-13 22:22:54 +0200

    up

--- app/meta-image.tsx
+++ app/meta-image.tsx
@@ -4,16 +4,21 @@ export const size = { width: 1200, height: 630 };
 export const alt = 'EtherscanLite';
 export const contentType = 'image/png';
 
-const handler = async () => {
-  const scheme = `http${process.env.NODE_ENV !== 'production' ? '' : 's'}`;
-  const logoUrl = `${scheme}://${process.env.VERCEL_URL}/img/etherscan-logo.png`;
+const handler = () => {
+  // const scheme = `http${process.env.NODE_ENV !== 'production' ? '' : 's'}`;
+  // const logoUrl = `${scheme}://${process.env.VERCEL_URL}/img/etherscan-logo.png`;
   return new ImageResponse(
     (
       <div
         tw={`flex flex-col justify-center items-center w-[${size.width}px] h-[${size.height}px]`}
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={logoUrl} alt={alt} width={815} height={183} />
+        <img
+          src="https://etherscanlite.vercel.app/img/etherscan-logo.png"
+          alt={alt}
+          width={815}
+          height={183}
+        />
         <h1 tw="text-8xl font-bold tracking-tight" style={{ color: '#21325b' }}>
           Simple Block Explorer
         </h1>
